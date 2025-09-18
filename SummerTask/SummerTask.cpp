448de65803977550cec60aa4eadbd3844b5acbf7
大和 松永 <yama.8010.pon@icloud.com>
2025-09-18 11:21:18 +0900

    エラー修正

--- SummerTask/SummerTask.cpp
+++ SummerTask/SummerTask.cpp
@@ -36,13 +36,33 @@ struct Enemy
 	bool Chest;
 	bool boss;
 };
+
+void drawMainCaracterParam(const MainCharacter& mc)
+{
+	cout << "名前 : " << mc.dynamicObject.name << endl
+		<< " 座標 : " << mc.dynamicObject.posX << ", " << mc.dynamicObject.posY
+		<< " 画像ID : " << mc.dynamicObject.imageID
+		<< " レベル : " << mc.LV
+		<< " 体力 : " << mc.HP
+		<< " マジックポイント : " << mc.MP
+		<< " 攻撃力 : " << mc.Power << endl << endl;
+}
 int main()
 {
 	Player player[] =
 	{
 		{
-			"P", 5, 8, 123, 5, 100, 150, 100, "a",
-		}
+			"P1", 5, 8, 123, 5, 100, 150, 100, //"a",
+		},
+		{
+			"P2", 7, 8, 124, 6, 120, 170, 110, //"b",
+		},
+		{
+			"P3", 10, 15, 125, 8, 150, 200, 130, //"c",
+		},
+		{
+			"P4", 10, 10, 126, 7, 100, 180, 120, //"d",
+		},
 	};
 	
 	Enemy enemy[] =
@@ -51,4 +71,17 @@ int main()
 			"E", 12, 6, 543, 6, 120, 110, 95, true, false,
 		}
 	};
+
+	MainCharacter mainCharacter[] =
+	{
+		{
+			"MC", 10, 10, 130, 8, 100, 150, 100,
+		},
+	};
+
+	int size = sizeof mainCharacter / sizeof * mainCharacter;
+	for (int i = 0; i < size; i++)
+	{
+		drawMainCaracterParam(mainCharacter[i]);
+	}
 }
