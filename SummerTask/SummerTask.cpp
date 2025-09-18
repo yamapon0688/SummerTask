82bc1e87f162d88f431e6e7f702c6a4e831a0c34
大和 松永 <yama.8010.pon@icloud.com>
2025-09-18 10:52:41 +0900

    プロジェクト ファイルを追加します。

---
+++ SummerTask/SummerTask.cpp
@@ -0,0 +1,57 @@
+﻿#include <iostream>
+using namespace std;
+
+struct DynamicObject
+{
+	char name[20];
+	int posX;
+	int posY;
+	int imageID;
+};
+
+struct MainCharacter
+{
+	DynamicObject dynamicObject;
+	int LV;
+	int HP;
+	int MP;
+	int Power;
+};
+
+struct NPC
+{
+	DynamicObject dynamicObject;
+	int talkID;
+};
+
+struct Player
+{
+	MainCharacter mainCharacter;
+	char job[20];
+};
+
+struct Enemy
+{
+	MainCharacter mainCharacter;
+	bool Chest;
+	bool boss;
+};
+int main()
+{
+	Player player[] =
+	{
+		{
+			"P", 5, 8, 123, 5, 100, 150, 100, "a",
+		}
+	};
+	
+	Enemy enemy[] =
+	{
+		{
+			"E", 12, 6, 543, 6, 120, 110, 95, true, false,
+		}
+	};
+}
+	
+
+}
